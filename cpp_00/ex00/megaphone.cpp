a8e98ddca7b10a1bcacf3d4652d52571ea125b7f
Octave Graf <cobra.morilles-0n@icloud.com>
2025-10-31 15:31:16 +0100

    no longer using forbidden functions (printf)

--- cpp_00/ex00/megaphone.cpp
+++ cpp_00/ex00/megaphone.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
-#include <cctype>
-
 int main(int argc, char **argv)
 {
 	{
 		if (argc < 2)
-			printf("* LOUD AND UNBEARABLE FEEDBACK NOISE *");
+			std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 		for (int i = 1; i < argc; i++)
 		{
 			std::string convert = argv[i];
 			std::transform(convert.begin(), convert.end(), convert.begin(), ::toupper);
-			printf("%s", convert.c_str());
+			std::cout << convert;
 		}
 	}
-	printf("\n");
+	std::cout << '\n';
 	return (0);
 }
