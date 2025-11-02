a5c88536e952378a1ae6e288c9e65545df88c68c
Octave Graf <cobra.morilles-0n@icloud.com>
2025-11-02 22:14:56 +0100

    started ex02

---
+++ cpp_00/ex02/Account.cpp
@@ -0,0 +1,36 @@
+#include "Account.hpp"
+
+Account::Account()
+{
+
+}
+
+int	Account::getNbAccounts()
+{
+	return (_nbAccounts);
+}
+
+int Account::getTotalAmount()
+{
+	return (_totalAmount);
+}
+
+int Account::getNbDeposits()
+{
+	return (_totalNbDeposits);
+}
+
+int Account::getNbWithdrawals()
+{
+	return (_totalNbWithdrawals);
+}
+
+void Account::displayAccountsInfos()
+{
+	return ;
+}
+
+void Account::_displayTimestamp()
+{
+	
+}
