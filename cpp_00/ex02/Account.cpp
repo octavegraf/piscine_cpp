78476e1aeac2b352ee636806ba37181a6371546b
Octave Graf <ocgraf@student.42.fr>
2025-11-07 16:38:07 +0100

    added functions

--- cpp_00/ex02/Account.cpp
+++ cpp_00/ex02/Account.cpp
@@ -1,8 +1,12 @@
 #include "Account.hpp"
 
-Account::Account()
+Account::Account(int initial_deposit) : _amount(initial_deposit), _nbDeposits(1), _nbWithdrawals(0)
 {
-
+	_nbAccounts++;
+	_amount += initial_deposit;
+	_totalAmount += initial_deposit;
+	_totalNbDeposits++;
+	_accountIndex = _nbAccounts - 1;
 }
 
 int	Account::getNbAccounts()
@@ -34,3 +38,22 @@ void Account::_displayTimestamp()
 {
 	
 }
+
+void Account::makeDeposit(int deposit)
+{
+	this->_amount += deposit;
+	this->_nbDeposits += 1;
+	this->_totalNbDeposits += 1;
+	this->_totalAmount += deposit; 
+}
+bool Account::makeWithdrawal(int withdrawal)
+{
+	this->_amount -= withdrawal;
+	this->_nbWithdrawals += 1;
+	this->_totalNbWithdrawals += 1;
+	this->_totalAmount -= withdrawal;
+}
+int Account::checkAmount(void) const
+{}
+void Account::displayStatus(void) const
+{}
