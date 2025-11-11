d24cac1b581b37508a289f2af2892a5f3a94da0d
Octave Graf <cobra.morilles-0n@icloud.com>
2025-11-11 11:58:25 +0100

    added printing for constructors, destructors, deposits, withdrawal and status. moved functions to correspond to headerfile order.

--- cpp_00/ex02/Account.cpp
+++ cpp_00/ex02/Account.cpp
@@ -1,13 +1,12 @@
 #include "Account.hpp"
+#include <iostream>
+#include <ctime>
+#include <sys/time.h>
 
-Account::Account(int initial_deposit) : _amount(initial_deposit), _nbDeposits(1), _nbWithdrawals(0)
-{
-	_nbAccounts++;
-	_amount += initial_deposit;
-	_totalAmount += initial_deposit;
-	_totalNbDeposits++;
-	_accountIndex = _nbAccounts - 1;
-}
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
 
 int	Account::getNbAccounts()
 {
@@ -31,29 +30,83 @@ int Account::getNbWithdrawals()
 
 void Account::displayAccountsInfos()
 {
-	return ;
+	_displayTimestamp();
+	std::cout << "accounts:" << getNbAccounts() << ";";
+	std::cout << "total:" << getTotalAmount() << ";";
+	std::cout << "deposits:" << getNbDeposits() << ";";
+	std::cout << "withdrawals:" << getNbWithdrawals() << std::endl;
+}
+Account::Account(int initial_deposit) : _amount(0), _nbDeposits(0), _nbWithdrawals(0)
+{
+	_nbAccounts++;
+	_amount += initial_deposit;
+	_totalAmount += initial_deposit;
+	_accountIndex = _nbAccounts - 1;
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";";
+	std::cout << "amount:" << _amount << ";";
+	std::cout << "created" << std::endl;
 }
 
-void Account::_displayTimestamp()
+Account::~Account()
 {
-	
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";";
+	std::cout << "amount:" << _amount << ";";
+	std::cout << "closed" << std::endl;
 }
 
 void Account::makeDeposit(int deposit)
 {
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";";
+	std::cout << "p_amount:" << _amount << ";";
+	std::cout << "deposit:" << deposit << ";";
 	this->_amount += deposit;
 	this->_nbDeposits += 1;
 	this->_totalNbDeposits += 1;
 	this->_totalAmount += deposit; 
+	std::cout << "amount:" << _amount << ";";
+	std::cout << "nb_deposits:" << _nbDeposits << std::endl;
 }
 bool Account::makeWithdrawal(int withdrawal)
 {
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";";
+	std::cout << "p_amount:" << _amount << ";";
+	std::cout << "withdrawal:";
+	if (withdrawal > _amount)
+	{
+		std::cout << "refused" << std::endl;
+		return (false);
+	}
+	std::cout << withdrawal << ";";
 	this->_amount -= withdrawal;
 	this->_nbWithdrawals += 1;
 	this->_totalNbWithdrawals += 1;
 	this->_totalAmount -= withdrawal;
+	std::cout << "amount:" << _amount << ";";
+	std::cout << "nb_withdrawals:" << _nbWithdrawals << std::endl;
+	return (true);
 }
 int Account::checkAmount(void) const
-{}
+{
+	return (_amount);
+}
 void Account::displayStatus(void) const
-{}
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";";
+	std::cout << "amount:" << _amount << ";";
+	std::cout << "deposits:" << _nbDeposits << ";";
+	std::cout << "withdrawals:" << _nbWithdrawals << std::endl;
+}
+
+void Account::_displayTimestamp()
+{
+	char format_time[20];
+	time_t current_time = time(NULL);
+	struct tm *simple_time = localtime(&current_time);
+	strftime(format_time, 20, "[%Y%m%d_%I%M%S]", simple_time);
+	std::cout << format_time << " ";
+}
