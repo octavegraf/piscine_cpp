f279b3385e752da8ad96d1bdaf69501a29e6894e
Octave Graf <ocgraf@student.42.fr>
2025-11-14 12:07:52 +0100

    improved error messages

--- cpp_01/ex04/main.cpp
+++ cpp_01/ex04/main.cpp
@@ -8,10 +8,10 @@ int replaceString(std::string fileName, std::string toFind, std::string toReplac
 	size_t pos = 0;
 
 	if (!file.is_open())
-		return (std::cerr << "Error: open." << std::endl, 1);
+		return (std::cerr << "Error: can't open " << fileName << std::endl, 1);
 	std::ofstream tempFile((fileName + ".temp").c_str());
 	if (!tempFile.is_open())
-		return (std::cerr << "Error: open." << std::endl, file.close(), 1);
+		return (std::cerr << "Error: can't open " << fileName << ".c_str" << std::endl, file.close(), 1);
 	while (file.get(c))
 		buffer += c;
 	while ((pos = buffer.find(toFind, pos)) != std::string::npos)
@@ -23,9 +23,9 @@ int replaceString(std::string fileName, std::string toFind, std::string toReplac
 	file.close();
 	tempFile.close();
 	if (std::remove(fileName.c_str()))
-		return (std::cerr << "Error: remove." << std::endl, 1);
+		return (std::cerr << "Error: can't remove " << fileName <<  std::endl, 1);
 	if (std::rename((fileName + ".temp").c_str(), fileName.c_str()))
-		return (std::cerr << "Error: rename." << std::endl, std::remove((fileName + ".tmp").c_str()), 1);
+		return (std::cerr << "Error: can't rename " << fileName << ".c_str" << std::endl, std::remove((fileName + ".tmp").c_str()), 1);
 	return (0);
 }
 
@@ -36,6 +36,5 @@ int main(int argc, char **argv)
 	if (argc == 4)
 		return (replaceString(argv[1], argv[2], argv[3]));
 	else
-		return (std::cerr << "Error: You need 3 arguments to use the program."
-			<< std::endl, 1);
+		return (std::cerr << "Error: You need 3 arguments to use the program." << std::endl, 1);
 }
