6bf2d8503fd24eb80fe49be501ec02c72d20219b
Octave Graf <ocgraf@student.42.fr>
2026-01-23 14:16:14 +0100

    simplified using directly static instead of pointer *=null

--- cpp_06/ex00/main.cpp
+++ cpp_06/ex00/main.cpp
@@ -2,18 +2,18 @@
 
 int main(int argc, char **argv)
 {
-	ScalarConverter *converter = NULL;
+	ScalarConverter converter;
 	if (argc == 2)
-		converter->convert(argv[1]);
+		converter.convert(argv[1]);
 	else
 	{
 		std::cout << "/!\\ If you want to make your own tests, run program with one argument." << std::endl;
-		converter->convert("A");
-		converter->convert("0");
-		converter->convert("nan");
-		converter->convert("42.0f");
-		converter->convert("42..0f");
-		converter->convert("2147483648");
-		converter->convert("42 a fait miroiter un GPU Nvidia pour au final jamais le donner");
+		converter.convert("A");
+		converter.convert("0");
+		converter.convert("nan");
+		converter.convert("42.0f");
+		converter.convert("42..0f");
+		converter.convert("2147483648");
+		converter.convert("42 a fait miroiter un GPU Nvidia pour au final jamais le donner");
 	}
 }
